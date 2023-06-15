05ebbad778cca465678a632f0bd508648abfbe5b
AYOUBSLAOUI1 <ayoub99slaoui@gmail.com>
2023-06-15 10:08:06 -0700

    my firdt C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * Description : this is my first c programme using puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
